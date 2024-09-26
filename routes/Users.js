015ad5a82d10dc91850eeb32283725e292c0a3d3
Jacek <jacuskozlowski@gmail.com>
2024-09-26 21:11:05 +0200

    feat: added JWT Auth

--- routes/Users.js
+++ routes/Users.js
@@ -1,31 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { Users } = require('../models');
-const bcrypt = require('bcryptjs'); // hash passwords
+const { Users } = require("../models");
+const bcrypt = require("bcryptjs"); // hash passwords
 
-router.post('/', async (req, res) => { // adding user to database
-    const { Username, Password } = req.body;
-    bcrypt.hash(Password, 10).then((hash) => {
-        Users.create({
-            Username: Username,
-            Password: hash,
-        })
-        res.json("success");
+const { sign } = require("jsonwebtoken"); // create token
+
+router.post("/", async (req, res) => {
+  // adding user to database
+  const { Username, Password } = req.body;
+  bcrypt.hash(Password, 10).then((hash) => {
+    Users.create({
+      Username: Username,
+      Password: hash,
     });
+    res.json("success");
+  });
 });
 
-router.post('/login', async (req, res) => { // checking if user exists in database
-    const { Username, Password } = req.body;
+router.post("/login", async (req, res) => {
+  // checking if user exists in database
+  const { Username, Password } = req.body;
 
-    const user = await Users.findOne({ where: { Username: Username } });
+  const user = await Users.findOne({ where: { Username: Username } });
 
-    if (!user) res.json({ error: 'User does not exist' });
+  if (!user) res.json({ error: "User does not exist" });
 
-    bcrypt.compare(Password, user.Password).then((match) => {
-        if (!match) res.json({ error: 'Wrong Username and Password Combination' });
+  bcrypt.compare(Password, user.Password).then((match) => {
+    if (!match) res.json({ error: "Wrong Username and Password Combination" });
 
-        res.json('You logged in!');
-    });
+    const accessToken = sign(
+      { Username: user.Username, id: user.id },
+      "importantsecret"
+    );
+    res.json(accessToken); // send token to frontend
+  });
 });
 
-module.exports = router; 
+module.exports = router;
