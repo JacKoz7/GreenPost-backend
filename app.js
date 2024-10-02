467d836400c24f690b3561682d152f33509bf4c5
Jacek <jacuskozlowski@gmail.com>
2024-10-02 18:37:00 +0200

    First commit

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(express.json()); // parse json data from request body
 app.use(cors()); // allow requests from any origin
 
-const db = require('./models');
+const db = require("./models");
 
 const PORT = process.env.PORT || 3001; // works for heroku
 
 // Routers
-const postRouter = require('./routes/Posts');
-app.use('/posts', postRouter);
-const commentRouter = require('./routes/Comments');
-app.use('/comments', commentRouter);
-const usersRouter = require('./routes/Users');
-app.use('/auth', usersRouter);
-const likesRouter = require('./routes/Likes');
-app.use('/likes', likesRouter);
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+const commentRouter = require("./routes/Comments");
+app.use("/comments", commentRouter);
+const usersRouter = require("./routes/Users");
+app.use("/auth", usersRouter);
+const likesRouter = require("./routes/Likes");
+app.use("/likes", likesRouter);
 
-db.sequelize.sync().then(() => {
-    app.listen(PORT, () => { 
-        console.log('Server is running on port 3001');      
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server is running on port 3001");
     });
-}); 
+  })
+  .catch((err) => {
+    console.error(err);
+  });
